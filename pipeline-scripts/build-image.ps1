3c5ed4af867de86d9dd3a54348b852a62c716d53
picccard <7443949+picccard@users.noreply.github.com>
2024-07-17 20:47:02 +0200

    added multiple dockerfiles with version in buildscript

--- pipeline-scripts/build-image.ps1
+++ pipeline-scripts/build-image.ps1
@@ -1,25 +1,52 @@
+
+[CmdletBinding()]
 param (
-    [string]$subscription = "landing-zone-demo-001",
-    [string]$registryName = "eulano",
-    [string]$imageName = "ghrunner-frompwsh-linux",
-    [string]$imageVersion = "v0.1.0",
-    [string]$runnerVersion = "2.317.0", # (Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/actions/runner/releases/latest').tag_name.TrimStart('v')
-    [string]$platform = "linux",
-    [string]$dockerfileDir = "./src/images"
+    [string]$Subscription = "landing-zone-demo-001",
+    [string]$RegistryName = "eulano",
+
+    [ValidateSet(
+        "ghrunner-frompwsh-linux",
+        "ghrunner-fromghcr-linux"
+    )]
+    [string]$ImageName = "ghrunner-frompwsh-linux",
+    [string]$RunnerVersion = "2.317.0", # (Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/actions/runner/releases/latest').tag_name.TrimStart('v')
+    [string]$Platform = "linux",
+    [string]$DockerfileDir = "src/images/",
+    [string]$VersionFilePath = "src/images/versions.json",
+    [ValidateSet(
+        "None",
+        "Major",
+        "Minor",
+        "Patch"
+    )]
+    [string]$UpdateType = "None"
 )
+$ErrorActionPreference = 'Stop'
+
+Import-Module "$PSScriptRoot/modules/version.psm1" -Force
 
 $validImageNamesToDockerfile = @{
-    "ghrunner-frompwsh-linux" = "Dockerfile.frompwsh"
-    "ghrunner-fromghcr-linux" = "Dockerfile.fromghcr"
+    "ghrunner-frompwsh-linux" = Join-Path -Path $DockerfileDir -ChildPath "Dockerfile.frompwsh"
+    "ghrunner-fromghcr-linux" = Join-Path -Path $DockerfileDir -ChildPath "Dockerfile.fromghcr"
 }
+$dockerfileName = $validImageNamesToDockerfile[$ImageName]
 
-if ($imageName -notin $validImageNamesToDockerfile.Keys) {
-    throw [System.ArgumentException]::new("Invalid image name. Valid image names are: $($validImageNamesToDockerfile.Keys -join ', ')")
-}
-$dockerfileName = $validImageNamesToDockerfile[$imageName]
 
-az account set --subscription $subscription
-az acr build -t "${imageName}:${imageVersion}" -t "${imageName}:latest" --registry $registryName --platform $platform --build-arg RUNNER_VERSION=$runnerVersion --file $dockerfileName $dockerfileDir
+# update image version
+$imageVersion = Get-CurrentVersion -ImageName $ImageName -Path $VersionFilePath
+$imageVersion = $UpdateType -eq "None" ? $imageVersion : (New-Version -UpdateType $UpdateType -Version $imageVersion)
+$null = Set-CurrentVersion -Version $imageVersion -ImageName $ImageName -Path $VersionFilePath
+
+
+Write-Debug @"
+az acr build -t "${ImageName}:v${imageVersion}" -t "${ImageName}:latest" --registry $RegistryName --Platform $Platform --build-arg RUNNER_VERSION=$RunnerVersion --file $dockerfileName $DockerfileDir
+"@
+
+az account set --subscription $Subscription
+az acr build -t "${ImageName}:v${imageVersion}" -t "${ImageName}:latest" --registry $RegistryName --platform $Platform --build-arg RUNNER_VERSION=$RunnerVersion --file $dockerfileName $DockerfileDir
+
+
+
 
 # https://github.com/dotnet/sdk/issues/39907
 
