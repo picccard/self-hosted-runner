4fcbf199d9e0111c48b19dd89b5b11c40a521f0e
Eskil Uhlving Larsen <7443949+picccard@users.noreply.github.com>
2024-07-30 17:30:46 +0200

    simplify image versioning

--- pipeline-scripts/build-image.ps1
+++ pipeline-scripts/build-image.ps1
@@ -3,27 +3,17 @@
 param (
     [string]$Subscription = "landing-zone-demo-001",
     [string]$RegistryName = "eulano",
-
     [ValidateSet(
         "ghrunner-frompwsh-linux",
         "ghrunner-fromghcr-linux"
     )]
     [string]$ImageName = "ghrunner-frompwsh-linux",
+    [string]$ImageVersion = "v0.1.0",
     [string]$RunnerVersion = "2.317.0", # (Invoke-RestMethod -Method Get -Uri 'https://api.github.com/repos/actions/runner/releases/latest').tag_name.TrimStart('v')
     [string]$Platform = "linux",
-    [string]$DockerfileDir = "src/images/",
-    [string]$VersionFilePath = "src/images/versions.json",
-    [ValidateSet(
-        "None",
-        "Major",
-        "Minor",
-        "Patch"
-    )]
-    [string]$UpdateType = "None"
+    [string]$DockerfileDir = "src/images/"
 )
 $ErrorActionPreference = 'Stop'
-
-Import-Module "$PSScriptRoot/modules/version.psm1" -Force
 
 $validImageNamesToDockerfile = @{
     "ghrunner-frompwsh-linux" = Join-Path -Path $DockerfileDir -ChildPath "Dockerfile.frompwsh"
@@ -31,16 +21,9 @@ $validImageNamesToDockerfile = @{
 }
 $dockerfileName = $validImageNamesToDockerfile[$ImageName]
 
-
-# update image version
-$imageVersion = Get-CurrentVersion -ImageName $ImageName -Path $VersionFilePath
-$imageVersion = $UpdateType -eq "None" ? $imageVersion : (New-Version -UpdateType $UpdateType -Version $imageVersion)
-$null = Set-CurrentVersion -Version $imageVersion -ImageName $ImageName -Path $VersionFilePath
-
-
 Write-Debug @"
-az acr build -t "${ImageName}:v${imageVersion}" -t "${ImageName}:latest" --registry $RegistryName --Platform $Platform --build-arg RUNNER_VERSION=$RunnerVersion --file $dockerfileName $DockerfileDir
+az acr build -t "${ImageName}:${ImageVersion}" -t "${ImageName}:latest" --registry $RegistryName --Platform $Platform --build-arg RUNNER_VERSION=$RunnerVersion --file $dockerfileName $DockerfileDir
 "@
 
 az account set --subscription $Subscription
-az acr build -t "${ImageName}:v${imageVersion}" -t "${ImageName}:latest" --registry $RegistryName --platform $Platform --build-arg RUNNER_VERSION=$RunnerVersion --file $dockerfileName $DockerfileDir
+az acr build -t "${ImageName}:${ImageVersion}" -t "${ImageName}:latest" --registry $RegistryName --platform $Platform --build-arg RUNNER_VERSION=$RunnerVersion --file $dockerfileName $DockerfileDir
